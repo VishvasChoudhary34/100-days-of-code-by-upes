a20e3a97b75d91ae51d9a7d2cc128d84ad4a53e5
VishvasChoudhary34 <uefjde@gmail.com>
2026-08-19 22:39:05 +0000

    Day 1 done

--- Day_001/Q1 (User Inputs, Operations & Output).c
+++ Day_001/Q1 (User Inputs, Operations & Output).c
@@ -1,1 +1,28 @@
+/* Q1: Write a program to input two numbers and display their sum.
 
+
+Sample Test Cases:
+Input 1:
+3 4
+Output 1:
+Sum = 7
+
+Input 2:
+-1 20
+Output 2:
+Sum = 19
+
+*/
+#include <stdio.h>
+int main()
+{
+    int a;
+    int b;
+    int sum;
+    printf("enter first number\n");
+    scanf("%d",&a);
+    printf("enter second number\n");
+    scanf("%d",&b);
+    sum = a+b;
+    printf("the sum of the 2 numbers is %d\n",sum);
+}
